e535f817d8ffe4d424166cbd221e245acb3de807
hocube <hocube159@gmail.com>
2024-07-16 20:45:56 +0900

    데이터 조회, UI 개선, 동적 라우팅

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,51 @@
 "use client";
+
 import { useRouter } from "next/navigation";
+import { supabase } from "@/utils/supabase";
 
 // Shadcn UI
 import { Button } from "@/components/ui/button";
+import { toast } from "@/components/ui/use-toast";
 
 // CSS
 import styles from "./page.module.scss";
-import { Router } from "lucide-react";
 
 function Home() {
 
   const router = useRouter();
+
+  // 페이지 생성 및 Supabase 연동
+  const onCreat = async () =>{
+    // Supabase 데이터베이스 row 생성
+    const {error, status} = await supabase
+      .from("todos")
+      .insert([
+        {
+          title: "",
+          start_date : new Date(),
+          end_date : new Date(),
+          contents: [],
+        }])
+      .select();
+
+    if(error){
+      console.log(error);
+    }
+
+    if (status === 201) {
+        toast({
+            title: "생성 완료!",
+            description: "새로운 투두리스트가 생성 되었습니다.",
+        });
+    }
+    
+    // 방금 생성한 TO DO LIST의 ID 값으로 URL 파라미터 생성
+        let { data } = await supabase.from("todos").select();
+
+        if (data) {
+            router.push(`/create/${data[data?.length - 1].id}`);
+        } else return;
+    };
 
   return (
     <div className={styles.container}>
@@ -22,7 +57,7 @@ function Home() {
         </div>
         {/*페이지 추가 버튼*/}
         <Button variant={'outline'} className="w-full bg-transparent text-orange-500 border-orange-400 hover:bg-orange-50 hover:text-orange-500" 
-        onClick={()=>router.push("/create")}
+        onClick={onCreat}
         >
           Add New Page
           </Button>
